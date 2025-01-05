809fe835c1ed0bdbb5ef3cd43294f22d7babd1e4
Cesar Velasquez <cova01072@gmail.com>
2025-01-05 17:34:59 -0400

    V6.0

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def ia(data_answers):
   [2,2,3,2,1,0,0],
   [2,2,3,2,2,0,0],
   [2,2,3,3,0,1,0],
-  [2,3,3,3,0,2,1],
-  [2,3,3,3,0,2,2],
-  [2,3,3,3,0,2,3],
+  [2,2,3,3,0,2,1],
+  [2,2,3,3,0,2,2],
+  [2,2,3,3,0,2,3],
   ]
     
   target=[
@@ -68,7 +68,6 @@ def ia(data_answers):
   #data_answers = [1,0,0,0,0,0,0]
   resultado = tree_classifier.predict([data_answers])
   convertido = np.ndarray.tolist(resultado)
-  
   return convertido
   
     
